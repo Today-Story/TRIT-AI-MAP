be0a4922e7e4562b5871bfbf9abebe7ce4f5e3aa
fallkim <fs728kimgc@naver.com>
2025-02-13 11:20:05 +0900

    fix : 포트설정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,11 +8,11 @@ import { CsvModule} from "./modules/csv/csv.module";
     imports: [
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: process.env.DB_HOST || 'localhost',
-            port: parseInt(process.env.DB_PORT, 10) || 5432,
-            username: process.env.DB_USER || 'postgres',
-            password: process.env.DB_PASSWORD || 'postgres',
-            database: process.env.DB_NAME || 'mydatabase',
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT, 10) ,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
             autoLoadEntities: true, // 엔티티를 자동으로 로드
             synchronize: true, // 개발 환경에서만 true (프로덕션 환경에서는 false)
             logging: true,
