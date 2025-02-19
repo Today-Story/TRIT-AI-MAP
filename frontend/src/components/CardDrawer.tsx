e7f55bb019577e94dfab7c71693279ee97ae504e
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-02-19 17:52:22 +0900

    feat: 지도 헤더 부분 퍼블리싱

--- frontend/src/components/CardDrawer.tsx
+++ frontend/src/components/CardDrawer.tsx
@@ -1,5 +1,10 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
+import { cn } from "@utils/cn";
+
+import { MdOutlineFilterList, MdOutlineKeyboardArrowDown } from "react-icons/md";
+
+import CategoryFilter from "./CategoryFilter";
 import ContentDetail from "./ContentDetail";
 import ContentSummary from "./ContentSummary";
 import { ContentData, DrawerMode } from "./GoogleMapComponent";
@@ -23,13 +28,13 @@ const CardDrawer: React.FC<CardDrawerProps> = ({
   onDrawerHeightChange,
 }) => {
   // 모드에 따라 높이 결정
-  let heightNum = 80; // collapsed
-  if (drawerMode === "summary") heightNum = 270;
-  if (drawerMode === "detail") heightNum = 600;
+  let height = "10%"; // collapsed
+  if (drawerMode === "summary") height = "30%";
+  if (drawerMode === "detail") height = "calc(100% - 200px)";
 
   // 모드 바뀔 때마다 상위에 알림
   useEffect(() => {
-    onDrawerHeightChange(heightNum);
+    // onDrawerHeightChange(height);
   }, [drawerMode]);
 
   // (1) collapsed
@@ -73,17 +78,31 @@ const CardDrawer: React.FC<CardDrawerProps> = ({
 
   return (
     <div
-      className={`
-        absolute bottom-0 left-0 w-full
-        bg-white rounded-t-3xl shadow-lg z-10
-        border border-gray-200
-        transition-all duration-300
-      `}
-      style={{ width: "390px", height: `${heightNum}px` }}
+      className={cn(
+        "fixed bottom-0 w-full max-w-mobile px-5 flex flex-col gap-5 justify-end border border-black translate-y-2/3",
+        drawerMode
+      )}
     >
-      {drawerMode === "collapsed" && renderCollapsed()}
-      {drawerMode === "summary" && renderSummary()}
-      {drawerMode === "detail" && renderDetail()}
+      <div className="flex gap-2 self-end">
+        <button
+          onClick={() => setDrawerMode("summary")}
+          className="flex gap-5 bg-primary-100 text-primary-300 rounded-full w-max py-2 px-4 border border-primary-200 items-center shadow-md shadow-primary-200"
+        >
+          PLAY
+          <MdOutlineKeyboardArrowDown size={24} />
+        </button>
+        <button className="flex items-center justify-center aspect-square h-10 bg-primary-100 rounded-full w-max border border-primary-200 shadow-md shadow-primary-200">
+          <MdOutlineFilterList size={24} color="#007FFF" />
+        </button>
+      </div>
+      <div
+        className="bg-white rounded-t-3xl shadow-lg z-10 transition-all duration-300 border border-black"
+        style={{ height }}
+      >
+        {/* {drawerMode === "collapsed" && renderCollapsed()} */}
+        {(drawerMode === "collapsed" || drawerMode === "summary") && renderSummary()}
+        {drawerMode === "detail" && renderDetail()}
+      </div>
     </div>
   );
 };
