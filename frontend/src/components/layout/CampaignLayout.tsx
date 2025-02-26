df0f509a51cc55f3c1dc623a44b37df35d01cc75
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-02-26 14:39:49 +0900

    fix: campaign layout 수정

--- frontend/src/components/layout/CampaignLayout.tsx
+++ frontend/src/components/layout/CampaignLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function CampaignLayout() {
   return (
-    <div className="max-w-mobile mx-auto h-screen">
+    <div className="max-w-mobile mx-auto h-screen pt-[60px] pb-[84px]">
       <Outlet />
     </div>
   );
